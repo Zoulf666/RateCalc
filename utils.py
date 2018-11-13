60ca66b953c0e83d1df05857a2284e894558e0fb
zoulf <zoulf666@163.com>
2018-11-13 17:22:44 +0800

    UI修改

---
+++ utils.py
@@ -0,0 +1,12 @@
+def center_root(root, width, height):
+    screenwidth = root.winfo_screenwidth()
+    screenheight = root.winfo_screenheight()
+    size = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+    return size
+
+
+def right_root(root, width, height, parent_width):
+    screenwidth = root.winfo_screenwidth()
+    screenheight = root.winfo_screenheight()
+    size = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2 + parent_width / 2, (screenheight - height) / 2)
+    return size
